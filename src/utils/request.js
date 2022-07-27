d38309475acdaf614198a8196b621552585544df
imurlobster <alexander2720@163.com>
2022-07-27 10:26:22 +0800

    feat: 2.首页-less的自动化导入

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ import router from '@/router'
 
 // 导出基准地址
 export const baseURL = 'http://pcapi-xiaotuxian-front-devtest.itheima.net'
-//export const baseURL = 'https://apipc-xiaotuxian-front.itheima.net'
+// export const baseURL = 'https://apipc-xiaotuxian-front.itheima.net'
 
 const instance = axios.create({
   // axios 的一些配置，baseURL  timeout
@@ -38,20 +38,20 @@ instance.interceptors.response.use(
   res => res.data,
   err => {
   // 401 状态码，进入该函数
-  if (err.response && err.response.status === 401) {
+    if (err.response && err.response.status === 401) {
     // 1. 清空无效用户信息
     // 2. 跳转到登录页
     // 3. 跳转需要传参（当前路由地址）给登录页码
-    store.commit('user/setUser', {})
-    // 当前路由地址
-    // 组件里头：`/user?a=10` $route.path === /user  $route.fullPath === /user?a=10
-    // js模块中：router.currentRoute.value.fullPath 就是当前路由地址，router.currentRoute 是ref响应式数据
-    const fullPath = encodeURIComponent(router.currentRoute.value.fullPath)
-    // encodeURIComponent 转换uri编码，防止解析地址出问题
-    router.push('/login?redirectUrl=' + fullPath)
-  }
-  return Promise.reject(err)
-})
+      store.commit('user/setUser', {})
+      // 当前路由地址
+      // 组件里头：`/user?a=10` $route.path === /user  $route.fullPath === /user?a=10
+      // js模块中：router.currentRoute.value.fullPath 就是当前路由地址，router.currentRoute 是ref响应式数据
+      const fullPath = encodeURIComponent(router.currentRoute.value.fullPath)
+      // encodeURIComponent 转换uri编码，防止解析地址出问题
+      router.push('/login?redirectUrl=' + fullPath)
+    }
+    return Promise.reject(err)
+  })
 
 // 请求工具函数
 export default (url, method, submitData) => {
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require('path')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
+
   devServer: {
     open: true
+  },
+
+  pluginOptions: {
+    'style-resources-loader': {
+      preProcessor: 'less',
+      patterns: [
+        path.join(__dirname, './src/assets/styles/variables.less'),
+        path.join(__dirname, './src/assets/styles/mixins.less')
+      ]
+    }
   }
 })
