3d444e7e774dedd7e8eb0dcb635b55508fc79a0b
imurlobster <alexander2720@163.com>
2022-08-04 17:24:17 +0800

    feat:25-首页主体-热门品牌

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -10,6 +10,8 @@
         <HomeNew />
         <!-- 人气推荐 -->
         <HomeHot />
+        <!-- 热门品牌 -->
+        <HomeBrand />
       </div>
     </div>
   </div>
@@ -19,14 +21,16 @@
 import HomeCategory from './components/home-category.vue'
 import HomeBanner from './components/home-banner.vue'
 import HomeNew from './components/home-new.vue'
-import HomeHot from './components/home-hot'
+import HomeHot from './components/home-hot.vue'
+import HomeBrand from './components/home-brand.vue'
 export default {
   name: 'PageHome',
   components: {
     HomeCategory,
     HomeBanner,
     HomeNew,
-    HomeHot
+    HomeHot,
+    HomeBrand
   }
 }
 </script>
